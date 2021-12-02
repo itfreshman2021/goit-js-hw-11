2b1f6865ac1a0602372cc66251c6c6306983a946
itfreshman2021 <85892626+itfreshman2021@users.noreply.github.com>
2021-12-02 10:42:50 +0000

    Deploying to gh-pages from @ itfreshman2021/goit-js-hw-11@4525b9a85ac124cde810d36d03215b656cc4705a 🚀

--- src.847d026b.js
+++ src.847d026b.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./css/styles.css");
 },{"./css/styles.css":"krre"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.847d026b.js.map
+//# sourceMappingURL=/goit-js-hw-11/src.847d026b.js.map
